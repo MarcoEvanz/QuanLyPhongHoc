fb1a92e5af36c2eadf8a3ed067acc0511b7b4f3b
duywin <128769273+duywin@users.noreply.github.com>
2024-03-12 23:23:17 +0700

    Thêm proceuder

--- database.sql
+++ database.sql
@@ -63,3 +63,92 @@ CREATE TABLE taikhoan (
     Password VARCHAR(20),
     LoaiTK VARCHAR(10)
 );
+DELIMITER //
+CREATE PROCEDURE InsertPhongHoc(
+    IN phonghoc_MaPh CHAR(10),
+    IN phonghoc_TenPh VARCHAR(50),
+    IN phonghoc_Succhua INT,
+    IN maUser CHAR(10)
+)
+BEGIN
+    INSERT INTO phonghoc (MaPh, TenPh, Succhua) VALUES (phonghoc_MaPh, phonghoc_TenPh, phonghoc_Succhua);
+    INSERT INTO log (MaUser, Noidung, ThoiGian) VALUES (maUser, CONCAT('Inserted a new PhongHoc: ', phonghoc_MaPh), NOW());
+END //
+DELIMITER ;
+-- updatePhongHoc
+DELIMITER //
+CREATE PROCEDURE UpdatePhongHoc(
+    IN phonghoc_MaPh CHAR(10),
+    IN phonghoc_TenPh VARCHAR(50),
+    IN phonghoc_Succhua INT,
+    IN maUser CHAR(10)
+)
+BEGIN
+    UPDATE phonghoc SET TenPh = phonghoc_TenPh, Succhua = phonghoc_Succhua WHERE MaPh = phonghoc_MaPh;
+    INSERT INTO log (MaUser, Noidung, ThoiGian) VALUES (maUser, CONCAT('Updated PhongHoc: ', phonghoc_MaPh), NOW());
+END //
+
+-- deletePhongHoc
+DELIMITER //
+CREATE PROCEDURE DeletePhongHoc(
+    IN maPh CHAR(10),
+    IN maUser CHAR(10)
+)
+BEGIN
+    DELETE FROM phonghoc WHERE MaPh = maPh;
+    INSERT INTO log (MaUser, Noidung, ThoiGian) VALUES (maUser, CONCAT('Deleted PhongHoc: ', maPh), NOW());
+END //
+
+-- insertGiaoVien
+DELIMITER //
+CREATE PROCEDURE InsertGiaoVien(
+    IN gv_MaGV CHAR(10),
+    IN gv_TenGV VARCHAR(50),
+    IN gv_NgaySinh DATE,
+    IN gv_DiaChi VARCHAR(100),
+    IN gv_SDT VARCHAR(15),
+    IN gv_SoNamCongTac INT,
+    IN gv_Username VARCHAR(20),
+    IN gv_Password VARCHAR(12),
+    IN maUser CHAR(10)
+)
+BEGIN
+    INSERT INTO giaovien (MaGV, TenGV, NgaySinh, DiaChi, SDT, SoNamCongTac, Username, Password) 
+    VALUES (gv_MaGV, gv_TenGV, gv_NgaySinh, gv_DiaChi, gv_SDT, gv_SoNamCongTac, gv_Username, gv_Password);
+    
+    -- Insert into the log table
+    INSERT INTO log (MaUser, Noidung, ThoiGian) VALUES (maUser, CONCAT('Inserted a new GiaoVien: ', gv_MaGV), NOW());
+END //
+
+-- updateGiaoVien
+DELIMITER //
+CREATE PROCEDURE UpdateGiaoVien(
+    IN gv_MaGV CHAR(10),
+    IN gv_TenGV VARCHAR(50),
+    IN gv_NgaySinh DATE,
+    IN gv_DiaChi VARCHAR(100),
+    IN gv_SDT VARCHAR(15),
+    IN gv_SoNamCongTac INT,
+    IN gv_Username VARCHAR(20),
+    IN gv_Password VARCHAR(12),
+    IN maUser CHAR(10)
+)
+BEGIN
+    UPDATE giaovien SET TenGV = gv_TenGV, NgaySinh = gv_NgaySinh, DiaChi = gv_DiaChi, SDT = gv_SDT, 
+    SoNamCongTac = gv_SoNamCongTac, Username = gv_Username, Password = gv_Password WHERE MaGV = gv_MaGV;
+    
+    -- Insert into the log table
+    INSERT INTO log (MaUser, Noidung, ThoiGian) VALUES (maUser, CONCAT('Updated GiaoVien: ', gv_MaGV), NOW());
+END //
+
+-- deleteGiaoVien
+DELIMITER //
+CREATE PROCEDURE DeleteGiaoVien(
+    IN maGV CHAR(10),
+    IN maUser CHAR(10)
+)
+BEGIN
+    DELETE FROM giaovien WHERE MaGV = maGV;
+    INSERT INTO log (MaUser, Noidung, ThoiGian) VALUES (maUser, CONCAT('Deleted GiaoVien: ', maGV), NOW());
+END //
+
