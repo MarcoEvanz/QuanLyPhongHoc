0d5533c7398ee2e6c87b2232d874097e9e40d7c9
duywin <128769273+duywin@users.noreply.github.com>
2024-03-17 19:15:30 +0700

    Update RegisterFrame.java

--- src/quanlyphonghoc/RegisterFrame.java
+++ src/quanlyphonghoc/RegisterFrame.java
@@ -1,6 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 package quanlyphonghoc;
 
@@ -8,114 +8,117 @@ package quanlyphonghoc;
  *
  * @author Admin
  */
-import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.JOptionPane;
+import java.awt.event.*;
+import javax.swing.*;
+public class Login extends javax.swing.JFrame {
 
-public class RegisterFrame extends JFrame {
-
-    private JLabel usernameLabel, passwordLabel, messageLabel, reconfirmPasswordLabel;
-    private JTextField usernameInput, reconfirmPasswordInput;
-    private JPasswordField passwordInput;
-    private JButton registerButton;
-    private Container container;
-
-    public RegisterFrame() {
-        setTitle("Registration Form");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(500, 100, 500, 350);
-
+    /**
+     * Creates new form Login
+     */
+    JLabel usernamelabel, passwordlabel;
+    JTextField usernameTextField;
+    JPasswordField passwordField;
+    JButton loginButton;
+    Container container;
+    /**
+     * Creates new form LoginFrame
+     */
+    public Login() {
+        usernamelabel = new JLabel("Username");
+        usernameTextField = new JTextField();
+        passwordlabel = new JLabel("Password");
+        passwordField = new JPasswordField();
+        loginButton = new JButton("login");
         container = getContentPane();
         container.setLayout(null);
-
-        setBound();
+        setBounds();
         addComponents();
-
-        setVisible(true);
+        addActionListener();
     }
-
-   public void setBound() {
-        messageLabel = new JLabel("");
-        messageLabel.setBounds(50, 10, 600, 30);
-
-        usernameLabel = new JLabel("Username:");
-        usernameLabel.setBounds(50, 50, 100, 30);
-
-        usernameInput = new JTextField();
-        usernameInput.setBounds(150, 50, 100, 30);
-
-        passwordLabel = new JLabel("Password:");
-        passwordLabel.setBounds(50, 100, 100, 30);
-
-        passwordInput = new JPasswordField();
-        passwordInput.setBounds(150, 100, 100, 30);
-
-        reconfirmPasswordLabel = new JLabel("Reconfirm Password:");
-        reconfirmPasswordLabel.setBounds(50, 150, 140, 30);
-
-        reconfirmPasswordInput = new JTextField();
-        reconfirmPasswordInput.setBounds(190, 150, 100, 30);
-
-        registerButton = new JButton("Register");
-        registerButton.setBounds(50, 200, 100, 30);
+    public void setBounds()
+    {
+        usernamelabel.setBounds(10,10,100,30);
+        usernameTextField.setBounds(100,10,200,30);
+        passwordlabel.setBounds(10,50,100,30);
+        passwordField.setBounds(100,50,200,30);
+        loginButton.setBounds(100,100,200,30);
     }
+    public void addComponents()
+    {
+       container.add(usernamelabel);
+       container.add(usernameTextField);
+       container.add(passwordlabel);
+       container.add(passwordField);
+       container.add(loginButton);
+    }
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">                          
+    private void initComponents() {
 
-    public void addComponents() {
-        container.add(messageLabel);
-        container.add(usernameLabel);
-        container.add(usernameInput);
-        container.add(passwordLabel);
-        container.add(passwordInput);
-        container.add(reconfirmPasswordLabel);
-        container.add(reconfirmPasswordInput);
-        container.add(registerButton);
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        registerButton.addActionListener(new ActionListener() {
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 300, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>                        
+private void addActionListener() {
+        loginButton.addActionListener(new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent e) {
-            String username = usernameInput.getText();
-            char[] passwordChars = passwordInput.getPassword();
-            String password = new String(passwordChars);
-            String confirmPassword = reconfirmPasswordInput.getText();
+            System.out.println("event called");
+            if (e.getSource() == loginButton) {
+                String username = usernameTextField.getText();
+                String password = passwordField.getText();
+                System.out.println(username + " " + password);
 
-            if (!password.equals(confirmPassword)) {
-                messageLabel.setText("Password does not match");
-                return;
-            }
+        AccountDao accountDao = new AccountDao();
+        String kq = accountDao.checkLogin(username, password);
 
-            AccountDao accountDao = new AccountDao();
-            int count = accountDao.countTaiKhoan();
-            String Mauser = "us" + count;
-            TaiKhoan taiKhoan = new TaiKhoan(Mauser, username, password);
-            taiKhoan.setLaoiTK("us");
-
-             // You should replace this with the appropriate way of creating an AccountDao instance
-
-            boolean success = accountDao.insertTaiKhoan(taiKhoan);
-
-            if (success) {
-                messageLabel.setText("Register completed");
-                new Login().setVisible(true);
+        if (kq!=null) {
+            if (kq.equals("ad")) {
+                System.out.println("Logged in as admin");
+                new AdminFrame(username).setVisible(true);
                 dispose();
-            } else {
-                messageLabel.setText("User existed");
+            } else if (kq.equals("gv")){
+                System.out.println("Logged in as giao vien");
+                new UserFrame(username).setVisible(true);
+                dispose();
             }
+        } else {
+            System.out.println("Unable to login");
+            JOptionPane.showMessageDialog(null, "User ID or password is wrong");
+            return;
         }
-       });
     }
-
-
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
+}
+        });
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+    SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                RegisterFrame frame = new RegisterFrame();
-                frame.setTitle("Student Register Form");
-                frame.setBounds(500, 100, 500, 350);
+                Login frame = new Login();
+                frame.setTitle("Login Form");
+                frame.setVisible(true);
+                frame.setBounds(250, 250, 370, 300);
                 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.setResizable(true);
+                frame.setResizable(false);
             }
         });
     }
-}
+    }
+    // Variables declaration - do not modify                     
+    // End of variables declaration                   
+
